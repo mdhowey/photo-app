275720af2ee48802ce714f26f0031e4975a93e2f
devhowey <dev.howey@gmail.com>
2021-10-04 18:29:22 -0500

    Set up routes for users; added view engine to server.js

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
+router.get('/edit', (req, res) => {
+    res.render('users/edit');
+});
+
+router.get('/index', (req, res) => {
+    res.render('users/index');
+});
+
+router.get('/new', (req, res) => {
+    res.render('users/new');
+});
+
+router.get('/show', (req, res) => {
+    res.render('users/show');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const app = express();
 
 const PORT = 4000;
 
-app.listen(PORT, (req, res) => {
-    console.log(`Listening on port ${PORT}`);
-});
+const app = express();
+
+app.set('view engine', 'ejs');
 
 require('./config/db.connection');
+const User = require('./models/User')
+const userRouter = require('./controllers/user_controllers')
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log(`[${req.url}] ${req.method} - ${new Date().toLocaleDateString()}`);
@@ -17,4 +21,10 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     console.log(`app.get '/' on port: ${PORT}`);
     res.send(`<h1>Test on '/' ==> it worked!</h1>`)
+});
+
+app.use('/users', userRouter);
+
+app.listen(PORT, (req, res) => {
+    console.log(`Listening on port ${PORT}`);
 });
