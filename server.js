2045ee245380e8765dd1284f768da463c7939d2d
devhowey <dev.howey@gmail.com>
2021-10-04 18:06:34 -0500

    set up index route

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const PORT = 4000;
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port ${PORT}`);
 });
+
+require('./config/db.connection');
+
+app.use((req, res, next) => {
+    console.log(`[${req.url}] ${req.method} - ${new Date().toLocaleDateString()}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    console.log(`app.get '/' on port: ${PORT}`);
+    res.send(`<h1>Test on '/' ==> it worked!</h1>`)
+});
